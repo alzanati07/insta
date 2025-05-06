12cb1c79cd802fb6769f927221f32387b8af5d36
alzanati07 <125388359+alzanati07@users.noreply.github.com>
2025-05-06 08:59:49 +0200

    Delete scraper directory

--- scraper/scraper.js
+++
@@ -1,9 +0,0 @@
-async function getStories(username) {
-  const stories = [
-    { id: 1, image: 'https://placekitten.com/400/600' },
-    { id: 2, image: 'https://placekitten.com/401/601' }
-  ];
-  return stories;
-}
-
-module.exports = { getStories };
