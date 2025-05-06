8f7144add1228cf9da1f1ba4ccba06d2df2a32a2
alzanati07 <125388359+alzanati07@users.noreply.github.com>
2025-05-06 08:26:59 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // <-- استدعاء CORS
 const { getStories } = require('../scraper/scraper');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // <-- تفعيل CORS
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/api/stories/:username', async (req, res) => {
