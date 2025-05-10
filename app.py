e4d39b335663b88884df816ccb6950245af31dac
alzanati07 <125388359+alzanati07@users.noreply.github.com>
2025-05-10 19:50:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,22 +17,9 @@ if ca_cert:
     except Exception as e:
         print(f"⚠️ فشل فك تشفير الشهادة: {e}")
 
-# ✅ تحميل بيانات الحساب والبروكسي من البيئة
+# ✅ تحميل بيانات الحساب من البيئة
 IG_USERNAME = os.getenv("INSTAGRAM_USERNAME")
 IG_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
-PROXY_HOST = os.getenv("PROXY_HOST")
-PROXY_PORT = os.getenv("PROXY_PORT")
-PROXY_USER = os.getenv("PROXY_USER")
-PROXY_PASS = os.getenv("PROXY_PASS")
-
-# ✅ إعداد البروكسي للبيئة
-if all([PROXY_USER, PROXY_PASS, PROXY_HOST, PROXY_PORT]):
-    proxy_url = f"http://{PROXY_USER}:{PROXY_PASS}@{PROXY_HOST}:{PROXY_PORT}"
-    os.environ["http_proxy"] = proxy_url
-    os.environ["https_proxy"] = proxy_url
-    print("✅ تم إعداد البروكسي")
-else:
-    print("⚠️ لم يتم ضبط إعدادات البروكسي بشكل صحيح")
 
 # ✅ إعداد Instaloader مع الشهادة
 L = instaloader.Instaloader()
